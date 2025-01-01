fe5c31f664f1e1ee17b0dd69d3637ab0140ddb77
lokeshwari0110 <lokeshwarijayakumar0110@gmail.com>
2025-01-01 16:57:16 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,3 +5,17 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+
+
+<script>
+        // Function to toggle the visibility of the Google Form
+        function toggleForm() {
+            var formContainer = document.getElementById("googleFormContainer");
+            if (formContainer.style.display === "none" || formContainer.style.display === "") {
+                formContainer.style.display = "block";  // Show the form
+            } else {
+                formContainer.style.display = "none";   // Hide the form
+            }
+        }
+    </script>
